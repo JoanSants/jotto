7ca14a1d2f570f69077d66e78b377934c57220e9
Joan Santos <joansants07@gmail.com>
2020-05-26 23:15:26 -0300

    Add code and tests for GuessedWords with words

--- src/GuessedWords/index.js
+++ src/GuessedWords/index.js
@@ -4,7 +4,21 @@ import PropTypes from 'prop-types';
 const GuessedWords = ({ guessedWords }) => (
 	<div data-test="component-guessed-words">
 		{guessedWords.length > 0 ? (
-			<div />
+			<div data-test="guessed-words">
+				<h3>Guessed Words</h3>
+				<table>
+					<thead>
+						<tr><th>Guess</th><th>Matching Letters</th></tr>
+					</thead>
+					<tbody>
+						{guessedWords.map(({ guessedWord, letterMatchCount }) => (
+							<tr key={guessedWord} data-test="guessed-word">
+								<td>{guessedWord}</td><td>{letterMatchCount}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		) : (
 			<span data-test="guess-instructions">
 				Try to guess the secret word!
