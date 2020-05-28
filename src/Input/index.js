b2065b64dda48ee65f135cb1677b364402d42140
Joan Santos <joansants07@gmail.com>
2020-05-28 19:30:01 -0300

    Input tests for success false condition

--- src/Input/index.js
+++ src/Input/index.js
@@ -3,12 +3,29 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
 	render() {
+		const { success } = this.props;
 		return (
-			<div />
+			<div data-test="component-input">
+				{!success && (
+					<form>
+						<input 
+							data-test="input-box"
+							type="text"
+							placeholder="emter guess"
+						/>
+						<button
+							data-test="submit-button"
+							type="submit"
+						>
+							Submit
+						</button>
+					</form>
+				)}
+			</div>
 		)
 	}
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ success }) => ({ success });
 
 export default connect(mapStateToProps)(Input);
